42e352bc7b3e8ce5be10f1b947c1e6e491dde884
HaTien <havantien95tb@gmail.com>
2018-09-03 14:00:32 +0700

    add file cylinder

---
+++ src/main/java/circle/cylinder/Cylinder.java
@@ -0,0 +1,30 @@
+package circle.cylinder;
+
+public class Cylinder  extends Circle{
+    private double height;
+
+    public double getHeight(){
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public Cylinder() {
+
+    }
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = height;
+    }
+
+    public double getArea() {
+        return getRadius() * getRadius() * 3.14;
+    }
+
+    public String toString() {
+        return "hinh tru co chieu cao la: " + getHeight();
+    }
+}
